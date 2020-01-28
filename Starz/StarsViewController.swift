422d019aa7b8fae8b1fb50b68897b44f46c7a513
Stephanie Ballard <user.name>
2020-01-27 21:02:44 -0800

    set up views and outlets

---
+++ Starz/StarsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Starz
+//
+//  Created by Stephanie Ballard on 1/27/20.
+//  Copyright © 2020 Stephanie Ballard. All rights reserved.
+//
+
+import UIKit
+
+class StarsViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var distanceTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    @IBAction func printStars(_ sender: UIButton) {
+    }
+    @IBAction func createStar(_ sender: UIButton) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
